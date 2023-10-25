37030ecf40eb7c5bc0c46bf9059ff9671b4c683b
Femosky <olufemiojeyemi@gmail.com>
2023-10-25 15:54:59 +0100

    Stopped at 1:15:27

--- src/layouts/SideBar.tsx
+++ src/layouts/SideBar.tsx
@@ -1,3 +1,30 @@
+import { Home } from 'lucide-react';
+import { buttonStyles } from '../components/Button';
+import { ElementType } from 'react';
+import { twMerge } from 'tailwind-merge';
+
 export function SideBar() {
-    return <div>Sidebar</div>;
+    return (
+        <aside className="sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 lg:hidden">
+            <SmallSidebarItem Icon={Home} title="Home" url="/" />
+        </aside>
+    );
+}
+
+type SmallSidebarItemProps = {
+    Icon: ElementType;
+    title: string;
+    url: string;
+};
+
+function SmallSidebarItem({ Icon, title, url }: SmallSidebarItemProps) {
+    return (
+        <a
+            href={url}
+            className={twMerge(buttonStyles({ variant: 'ghost' }), 'py-4 px-1 flex flex-col items-center rounded-lg')}
+        >
+            <Icon className="w-6 h-6" />
+            <div className="text-sm">{title}</div>
+        </a>
+    );
 }
